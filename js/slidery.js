2bccae6ab823aad0aa599e94850f21777781643f
karol509x <karol509x@gmail.com>
2017-08-26 16:09:31 +0200

    slidery

---
+++ js/slidery.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(function(){
+    //zmienne
+    var slider = $('#slider');
+    var slideshow = $('.slide-show');
+    var slideCount = slide-show.children().length;
+    var slideWidth = 100/slideCount;
+    var slideIndex = 0;
+});
